429177934530e49af6907e71e065039ed4787ef5
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-08-17 19:18:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-//your JS code here. If required.
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function updateRow(row, time) {
+function updateRow(row, name, time) {
     const cells = row.cells;
+    cells[0].textContent = name;
     cells[1].textContent = time.toFixed(3);
 }
 
 const outputTable = document.getElementById('output');
+
+const loadingRow = outputTable.insertRow();
+const loadingCell = loadingRow.insertCell(0);
+loadingCell.colSpan = 2;
+loadingCell.textContent = 'Loading...';
 
 const promises = [
     wait(Math.random() * 2000 + 1000),
@@ -18,24 +23,16 @@ const promises = [
 
 Promise.all(promises)
     .then(times => {
+        outputTable.deleteRow(loadingRow.rowIndex);
+
         const promiseNames = ['Promise 1', 'Promise 2', 'Promise 3'];
         for (let i = 0; i < promises.length; i++) {
             const newRow = outputTable.insertRow();
-            const nameCell = newRow.insertCell(0);
-            const timeCell = newRow.insertCell(1);
-
-            nameCell.textContent = promiseNames[i];
-            updateRow(newRow, times[i] / 1000);
+            updateRow(newRow, promiseNames[i], times[i] / 1000);
         }
 
         const totalTime = times.reduce((acc, time) => acc + time, 0) / 1000;
         const totalRow = outputTable.insertRow();
-        const totalNameCell = totalRow.insertCell(0);
-        const totalTimeCell = totalRow.insertCell(1);
-
-        totalNameCell.textContent = 'Total';
-        updateRow(totalRow, totalTime);
-        
-        document.querySelector("tbody tr:nth-child(1)").remove();
+        updateRow(totalRow, 'Total', totalTime);
     })
     .catch(error => console.error(error));
